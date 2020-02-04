5823d48af453958abe736a322eb607de5272a32b
Sergey Ganin <mcheguevara2@gmail.com>
2020-02-04 17:25:38 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const { exec } = require("child_process");
 const filePath = "test";
-
+const jsonxml = require("jsontoxml");
+const convert = require("xml-js");
+console.log({ convert });
 /**
  * @param {string} filePath with out extension
  * @param {function} log loging callback function
@@ -10,23 +12,10 @@ const filePath = "test";
 module.exports = function JSONAndYAML(filePath = "test", log = console.log) {
   const transform = (filename, cb) => {
     if (filename.indexOf(".json") !== -1) {
-      exec(
-        `"./node_modules/.bin/json2yaml" ./${filePath}.json > ./${filePath}.yaml`,
-        err => {
-          if (err) console.log(err);
-          console.log(`${filePath}.json > yaml`);
-          cb();
-        }
-      );
+      json_to_yaml(filePath, cb);
+      json_to_xml(filePath, cb);
     } else {
-      exec(
-        `"./node_modules/.bin/yaml2json" ./${filePath}.yaml > ./${filePath}.json`,
-        err => {
-          if (err) console.log(err);
-          console.log(`${filePath}.yaml > json`);
-          cb();
-        }
-      );
+      yaml_to_json(filePath, cb);
     }
   };
   const watchers = {
@@ -51,3 +40,46 @@ module.exports = function JSONAndYAML(filePath = "test", log = console.log) {
     }
   }
 };
+
+// module.exports = function multiFormatConverter({
+//   fileName = "test",
+//   formats = ["yaml", "json", "xml"],
+//   log = console.log
+// }) {
+//   const transform = (filename, cb) => {
+//     const fileExtension = filename.split(".").slice(-1);
+//     formats.filter();
+//   };
+// };
+
+function yaml_to_json(fileName, cb) {
+  const [name, extension] = fileName.split(".");
+  exec(
+    `"./node_modules/.bin/yaml2json" ./${name}.yaml > ./${name}.json`,
+    err => {
+      if (err) console.log(err);
+      console.log(`${name}.yaml > json`);
+      cb();
+    }
+  );
+}
+
+function json_to_yaml(fileName, cb) {
+  const [name, extension] = fileName.split(".");
+  exec(
+    `"./node_modules/.bin/json2yaml" ./${name}.json > ./${name}.yaml`,
+    err => {
+      if (err) console.log(err);
+      console.log(`${name}.json > yaml`);
+      cb();
+    }
+  );
+}
+
+function json_to_xml(fileName, cb) {
+  const [name, extension] = fileName.split(".");
+  const obj = JSON.parse(fs.readFileSync(`./${name}.json`));
+  const result = convert.js2xml(obj, { compact: true, spaces: 4 });
+  console.log({ obj, result });
+  fs.writeFile(`./${name}.xml`, result, console.log);
+}
